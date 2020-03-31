2ad593f746c5d97048ec1f19574e931ba953c104
Twitty <btwitty187@gmail.com>
2020-03-30 22:40:36 -0500

    dumb things because nebular sucks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {NbComponentSize, NbMenuItem} from "@nebular/theme";
 import {Owner} from "./owners/create-owner/_models/owner";
 import {OwnerService} from "./shared/services/owner.service";
@@ -9,14 +9,15 @@ import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Route
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit{
-  owners: Owner[];
+  allOwners: Owner[];
   selectedOwner: Owner = new Owner();
   sizes: NbComponentSize[] = ['medium'];
   fullname: string;
-
+  storeOwnerId: number;
   constructor(public ownersService: OwnerService,  private http: HttpClient, private router: Router) {
 
   }
@@ -24,47 +25,33 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.getOwners();
-    this.fullname = this.selectedOwner.getFullName();
+    //this.fullname = this.selectedOwner.getFullName();
+
   }
   getOwners() {
-    this.owners = [];
+    this.allOwners = [];
     this.ownersService.getOwners().subscribe((data: []) => {
       console.log(data);
-      this.owners = data;
+      this.allOwners = data;
     });
+    this.selectedOwner = this.allOwners[1];
+    console.log(this.storeOwnerId + 'test')
   }
-  onChangeObj(data){
-    this.selectedOwner = data;
-    console.log(this.selectedOwner);
-    return this.selectedOwner;
+  onChangeObj(newOwner) {
+    console.log(newOwner);
+    this.selectedOwner = newOwner;
+    console.log(this.selectedOwner.ID);
+    this.storeOwnerId = this.selectedOwner.ID;
+
   }
-  title = 'med-app';
-  items: NbMenuItem[] = [
-    {
-      title: 'Stores',
-      link: 'list-stores/',
-      queryParams: {storeOwnerId: this.selectedOwner.ID},
-      icon: 'person-outline'
-    },
-    {
-      title: 'Appointments',
-      link: 'dashboard',
-      icon: 'person-outline'
-    },
-    {
-      title: 'Reports',
-      link: 'dashboard',
-      icon: 'person-outline'
-    },
-    {
-      title: 'Get All Owners',
-      link: 'list-owners',
-      icon: 'person-outline'
-    }, {
-      title: 'dashboard',
-      link: 'dashboard',
-      icon: 'person-outline'
-    }
-  ];
+  getOwnerDetails(ownerId: any) {
+    this.router.navigate((['/get-owner/', ownerId])).then(r =>
+      console.log('next task'));
+  }
+  gotoOwnersStores(storeOwnerId: any){
+    this.router.navigate(['/list-stores/', storeOwnerId]).then(r =>
+    console.log('this.selectedOwner'));
+  }
+
 
 }
